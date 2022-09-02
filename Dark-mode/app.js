7451baa12e2babfa510ccea09e64f353f9bf9aec
Garvit Yadav <48787184+garvityadav@users.noreply.github.com>
2022-09-02 14:45:37 +0530

    update

--- Dark-mode/app.js
+++ Dark-mode/app.js
@@ -1,1 +1,22 @@
-console.log('dark mode');
+const button = document.querySelector(".btn");
+const heading = document.querySelector("h2");
+const article = document.querySelector(".articles");
+
+button.addEventListener("click", () => {
+  document.documentElement.classList.toggle("dark-theme");
+});
+
+let newArticle = articles.map((items) => {
+   const {title, date, length,snippet} = items;
+   return `<section class="articles">
+   <article class="post">
+     <h2>${title}</h2>
+     <div class="post-info">
+       <span>${new Date(date).toDateString()}</span> <span>${length} min read</span>
+       </div>
+        <p>${snippet}</p>
+      </article>
+    </section>`
+}).join('');
+
+article.innerHTML = newArticle;
--- Filters/app.js
+++ Filters/app.js
@@ -1,1 +1,70 @@
-console.log('filters project');
+const container = document.querySelector(".products-container");
+const forms = document.querySelector(".input-form");
+const search = document.querySelector(".search-input");
+const companies = document.querySelector(".companies");
+const companyButton = document.querySelector(".company-btn");
+
+// Display Products
+function displayProducts(product) {
+  if (product.length < 1) {
+    return (container.innerHTML =
+      "<h6>Sorry no products matched the search </h6>");
+  }
+  let display = product
+    .map((items) => {
+      const { image, company, title, price } = items;
+      return `<article class="product">
+    <img src="${image}" alt="" class="product-img img">
+    <footer>
+    <h5 class="product-name">${company}</h5>
+    <span class="product-price">$${price}</span>
+    </footer>
+    </article>`;
+    })
+    .join("");
+
+  container.innerHTML = display;
+}
+
+//Display Button
+function displayButtons() {
+  let set = ["all", ...new Set(products.map((items) => items.company))];
+  companies.innerHTML = set
+    .map((item) => {
+      return `<button class="company-btn" data-id="${item}">${item}</button>`;
+    })
+    .join("");
+}
+
+//To Filter Product
+function filterProducts(keyWord) {
+  let searchItems = products.filter((items) => {
+    if (
+      items.title.toLowerCase().includes(keyWord.toLowerCase()) ||
+      items.company.toLowerCase().includes(keyWord.toLowerCase())
+    ) {
+      return true;
+    }
+  });
+  displayProducts(searchItems);
+}
+
+//event Listener on search
+search.addEventListener("keyup", () => {
+  const inputValue = search.value;
+  filterProducts(inputValue);
+});
+
+//event listener on companies filter
+companies.addEventListener("click", (e) => {
+  let el = e.target;
+  if (el.classList.contains("company-btn")) {
+    if (el.dataset.id.toLowerCase() === "all") {
+      return displayProducts(products);
+    } else {
+      return filterProducts(el.dataset.id);
+    }
+  }
+});
+
+displayButtons();
